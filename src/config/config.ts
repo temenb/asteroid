d5f8302ea26a161a25874fe6ff10e4c10d08b592
Oleksii Temchenko <temenb@gmail.com>
2025-08-20 22:15:54 +0300

    stable

--- src/config/config.ts
+++ src/config/config.ts
@@ -8,6 +8,7 @@ export  const config = {
     refreshTokenSecret: process.env.JWT_REFRESH_SECRET!,
     accessTokenExpiresIn: process.env.ACCESS_TOKEN_EXPIRES_IN || '15m',
     refreshTokenExpiresIn: process.env.REFRESH_TOKEN_EXPIRES_IN || '7d',
+    rabbitmqQueueProfileCreated: process.env.RABBITMQ_QUEUE_PROFILE_CREATED || 'profile.created',
 };
 
 
