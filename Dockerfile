552dbf5dd5d76e81e8b9819ddc04f403b7f22a6e
Oleksii Temchenko <temenb@gmail.com>
2025-08-25 23:53:41 +0300

    refactoring is done. Kafka is next

--- Dockerfile
+++ Dockerfile
@@ -13,8 +13,14 @@ COPY services/asteroid/tsconfig.json ./services/asteroid/
 COPY services/asteroid/src ./services/asteroid/src/
 COPY services/asteroid/prisma ./services/asteroid/prisma/
 COPY services/asteroid/__tests__ ./services/asteroid/__tests__/
+COPY services/asteroid/.env ./services/asteroid/.env
 
 USER root
+
+RUN apt-get clean && \
+    mkdir -p /var/lib/apt/lists/partial && \
+    apt-get update && \
+    apt-get install -y netcat-openbsd
 
 RUN corepack enable && pnpm install
 RUN chown -R node:node /usr/src/app
@@ -23,10 +29,11 @@ USER node
 
 RUN pnpm --filter @shared/logger build
 RUN pnpm --filter asteroid build
+RUN pnpm --filter asteroid generate
 
 EXPOSE 3000
 
 CMD ["pnpm", "--filter", "asteroid", "start"]
 
 HEALTHCHECK --interval=10s --timeout=3s --start-period=5s --retries=3 \
-  CMD curl -f http://localhost:3000/health || exit 1
+  CMD nc -z localhost 3000 || exit 1
--- src/services/asteroid.service.ts
+++ src/services/asteroid.service.ts
@@ -1,39 +1,39 @@
-// import { PrismaClient } from '@prisma/client';
-// import config from '../config/config';
-// import {broadcastEvent} from "../utils/kafka";
-//
-// const prisma = new PrismaClient();
-//
-// export async function findAsteroid(ownerId: string) {
-//   return prisma.asteroid.findFirst({ where: { ownerId } });
-// }
-//
-//
-// export async function upsertAsteroid(ownerId: string) {
-//
-//   const existing = await prisma.asteroid.findFirst({ where: { ownerId } });
-//
-//   if (existing) {
-//     return existing;
-//   }
-//
-//   const asteroid = await prisma.asteroid.create({
-//     data: {
-//       ownerId: ownerId,
-//       galaxyId: 'galaxy-456',
-//       asteroidType: 'BASE',
-//       ownerType: 'PLAYER',
-//       resourceType: 'IRON',
-//       asteroidGrade: 'EPIC',
-//       axisX: 42,
-//       axisY: 17,
-//       health: 100,
-//       // name: 'Zeta-9',
-//     },
-//   });
-//
-//
-//   broadcastEvent(config.kafkaTopicAsteroidCreated, [{ value: JSON.stringify({ id: asteroid.id }) }]);
-//
-//   return asteroid;
-// }
+import { PrismaClient } from '@prisma/client';
+import config from '../config/config';
+import {broadcastEvent} from "../utils/kafka";
+
+const prisma = new PrismaClient();
+
+export async function findAsteroid(ownerId: string) {
+  return prisma.asteroid.findFirst({ where: { ownerId } });
+}
+
+
+export async function upsertAsteroid(ownerId: string) {
+
+  const existing = await prisma.asteroid.findFirst({ where: { ownerId } });
+
+  if (existing) {
+    return existing;
+  }
+
+  const asteroid = await prisma.asteroid.create({
+    data: {
+      ownerId: ownerId,
+      galaxyId: 'galaxy-456',
+      asteroidType: 'BASE',
+      ownerType: 'PLAYER',
+      resourceType: 'IRON',
+      asteroidGrade: 'EPIC',
+      axisX: 42,
+      axisY: 17,
+      health: 100,
+      // name: 'Zeta-9',
+    },
+  });
+
+
+  broadcastEvent(config.kafkaTopicAsteroidCreated, [{ value: JSON.stringify({ id: asteroid.id }) }]);
+
+  return asteroid;
+}
